e3b3252c1e600a5e29f61773108e7c7d4e8ce5ea
Jonas Sannewald <github.zgxjfq@sannewald.email>
2025-02-13 23:58:50 +0100

    AuthTypes umbenannt

--- Sources/OEPNVWalletPluginAPI/OEPNVWalletPluginAuthType.swift
+++ Sources/OEPNVWalletPluginAPI/OEPNVWalletPluginAuthType.swift
@@ -8,18 +8,18 @@
 import Foundation
 
 /// Repräsentiert die Art, wie sich das Plugin beim Verkehrsverbund authentifiziert.
-public enum OEPNVWalletPluginAuthType {
+public enum OEPNVWalletPluginAuthType: Codable {
     
     /// Authentifizierung über E-Mail-Adresse und Passwort.
-    case emailAndPassword
+    case emailPassword
     
     /// Authentifizierung über Benutzername und Passwort.
-    case usernameAndPassword
+    case usernamePassword
     
     /// Authentifizierung über E-Mail-Adresse und Passwort, zusätzlich 2. Faktor via TOTP.
-    case emailAndPasswordAndTOTP
+    case emailPasswordTOTP
     
     /// Authentifizierung über Benutzername und Passwort, zusätzlich 2. Faktor via TOTP.
-    case usernameAndPasswordAndTOTP
+    case usernamePasswordTOTP
     
 }
