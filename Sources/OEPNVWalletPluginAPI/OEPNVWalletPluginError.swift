54a6edd914373a5c381be98bce49c50a9437db31
Jonas Sannewald <github.zgxjfq@sannewald.email>
2025-04-06 18:19:06 +0200

    PluginErrors angepasst

--- Sources/OEPNVWalletPluginAPI/OEPNVWalletPluginError.swift
+++ Sources/OEPNVWalletPluginAPI/OEPNVWalletPluginError.swift
@@ -16,13 +16,16 @@ public enum OEPNVWalletPluginError: Error {
     /// Die Verbindung zum Verkehrsverbund ist fehlgeschlagen, z.B. weil die Server des Verkehrsverbunds nicht erreichbar sind.
     case connectionFailed(description: String)
     
+    /// Der API-Endpunkt gibt einen Fehler zurück, z.B. wegen einer fehlerhaften Anfrage.
+    case endpointFailed(description: String)
+    
     /// Die Authentifizierung ist fehlgeschlagen, z.B. wegen fehlerhaften Anmeldedaten.
     case authenticationFailed(description: String)
     
     /// Das Parsen der Daten des Verkehrsverbunds ist fehlgeschlagen, z.B. weil sich die JSON-Struktur verändert hat.
     case parsingFailed(description: String)
     
-    /// Es ist ein interner Fehler aufgetreten, z.B. durch unerwartete Daten.
+    /// Es ist ein interner Fehler aufgetreten, z.B. durch einen fehlerhaften internen Zustand im Plugin.
     case internalFailed(description: String)
     
     /// Weitergabe eines fremden Fehlers, z.B. von einer Drittbibliothek.
