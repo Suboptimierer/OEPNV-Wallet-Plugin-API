3fad8311f4a166d85c5f869901a2805dbcc1913b
Jonas Sannewald <github.zgxjfq@sannewald.email>
2025-04-17 03:09:55 +0200

    Bug fix

--- Sources/OEPNVWalletPluginAPI/OEPNVWalletPluginError.swift
+++ Sources/OEPNVWalletPluginAPI/OEPNVWalletPluginError.swift
@@ -14,22 +14,58 @@ import Foundation
 public enum OEPNVWalletPluginError: Error {
     
     /// Die Verbindung zum Verkehrsverbund ist fehlgeschlagen, z.B. weil die Server des Verkehrsverbunds nicht erreichbar sind.
-    case connectionFailed(description: String, file: String = #fileID, function: String = #function, line: Int = #line)
+    case connectionFailed(
+        type: String = "connectionFailed",
+        description: String,
+        file: String = #fileID,
+        function: String = #function,
+        line: Int = #line
+    )
     
     /// Der API-Endpunkt gibt einen Fehler zurück, z.B. wegen einer fehlerhaften Anfrage.
-    case endpointFailed(description: String, file: String = #fileID, function: String = #function, line: Int = #line)
+    case endpointFailed(
+        type: String = "endpointFailed",
+        description: String,
+        file: String = #fileID,
+        function: String = #function,
+        line: Int = #line
+    )
     
     /// Die Authentifizierung ist fehlgeschlagen, z.B. wegen fehlerhaften Anmeldedaten.
-    case authenticationFailed(description: String, file: String = #fileID, function: String = #function, line: Int = #line)
+    case authenticationFailed(
+        type: String = "authenticationFailed",
+        description: String,
+        file: String = #fileID,
+        function: String = #function,
+        line: Int = #line
+    )
     
     /// Das Parsen der Daten des Verkehrsverbunds ist fehlgeschlagen, z.B. weil sich die JSON-Struktur verändert hat.
-    case parsingFailed(description: String, file: String = #fileID, function: String = #function, line: Int = #line)
+    case parsingFailed(
+        type: String = "parsingFailed",
+        description: String,
+        file: String = #fileID,
+        function: String = #function,
+        line: Int = #line
+    )
     
     /// Es ist ein interner Fehler aufgetreten, z.B. durch einen fehlerhaften internen Zustand im Plugin.
-    case internalFailed(description: String, file: String = #fileID, function: String = #function, line: Int = #line)
+    case internalFailed(
+        type: String = "internalFailed",
+        description: String,
+        file: String = #fileID,
+        function: String = #function,
+        line: Int = #line
+    )
     
     /// Weitergabe eines fremden Fehlers, z.B. von einer Drittbibliothek.
-    case underlyingFailed(description: String, file: String = #fileID, function: String = #function, line: Int = #line)
+    case underlyingFailed(
+        type: String = "underlyingFailed",
+        description: String,
+        file: String = #fileID,
+        function: String = #function,
+        line: Int = #line
+    )
     
 }
 
@@ -38,17 +74,14 @@ extension OEPNVWalletPluginError: LocalizedError {
     
     /// Einheitliche Fehler-Beschreibung von Plugins.
     public var errorDescription: String? {
-        let fullType = String(describing: self)
-        let parsedType = fullType.components(separatedBy: "(").first ?? fullType
-        
         switch self {
-        case let .connectionFailed(description, file, function, line),
-             let .endpointFailed(description, file, function, line),
-             let .authenticationFailed(description, file, function, line),
-             let .parsingFailed(description, file, function, line),
-             let .internalFailed(description, file, function, line),
-             let .underlyingFailed(description, file, function, line):
-            return "Plugin-Fehler \"\(parsedType)\" (\(function) @ \(file):\(line)): \(description)"
+        case let .connectionFailed(type, description, file, function, line),
+             let .endpointFailed(type, description, file, function, line),
+             let .authenticationFailed(type, description, file, function, line),
+             let .parsingFailed(type, description, file, function, line),
+             let .internalFailed(type, description, file, function, line),
+             let .underlyingFailed(type, description, file, function, line):
+            return "Plugin-Fehler \"\(type)\" (\(function) @ \(file):\(line)): \(description)"
         }
     }
     
